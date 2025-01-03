86a04274093af9f2cfb69d403eb0e4b7e0796b3c
Daniel Au <daniel.au05@gmail.com>
2025-01-02 16:11:28 -0800

    Revert Vercel insights

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <SpeedInsights />
   </StrictMode>
 );
